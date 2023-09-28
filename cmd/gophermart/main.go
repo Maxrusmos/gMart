7b66592186158ba6863deb68a1537cd56771509e
md.vorobev <md.vorobev@i-teco.ru>
2023-09-28 17:31:27 +0300

    clone

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,45 +1,47 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"gMart/cmd/gophermart/internal/database"
 	"gMart/cmd/gophermart/internal/user"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	// var (
-	// 	runAddressFlag           = flag.String("a", "localhost:8080", "Address and port to run the service")
-	// 	databaseURIFlag          = flag.String("d", "user=postgres password=490Sutud dbname=gofermartUsers sslmode=disable", "Database connection URI")
-	// 	accrualSystemAddressFlag = flag.String("r", "", "Address of the accrual system")
-	// )
+	var (
+		runAddressFlag           = flag.String("a", "localhost:8080", "Address and port to run the service")
+		databaseURIFlag          = flag.String("d", "user=postgres password=490Sutud dbname=gofermartUsers sslmode=disable", "Database connection URI")
+		accrualSystemAddressFlag = flag.String("r", "", "Address of the accrual system")
+	)
 
-	// flag.Parse()
+	flag.Parse()
 
-	// if *runAddressFlag != "" {
-	// 	viper.Set("run_address", *runAddressFlag)
-	// }
-	// if *databaseURIFlag != "" {
-	// 	viper.Set("database_uri", *databaseURIFlag)
-	// }
-	// if *accrualSystemAddressFlag != "" {
-	// 	viper.Set("accrual_system_address", *accrualSystemAddressFlag)
-	// }
+	if *runAddressFlag != "" {
+		viper.Set("run_address", *runAddressFlag)
+	}
+	if *databaseURIFlag != "" {
+		viper.Set("database_uri", *databaseURIFlag)
+	}
+	if *accrualSystemAddressFlag != "" {
+		viper.Set("accrual_system_address", *accrualSystemAddressFlag)
+	}
 
-	// viper.SetEnvPrefix("LOYALTY")
-	// viper.AutomaticEnv()
+	viper.SetEnvPrefix("LOYALTY")
+	viper.AutomaticEnv()
 
-	// viper.SetDefault("run_address", ":8080")
-	// viper.SetDefault("database_uri", "postgres://postres:490Sutud@localhost:5432/gofermartUsers")
-	// viper.SetDefault("accrual_system_address", "http://localhost:8000")
+	viper.SetDefault("run_address", ":8080")
+	viper.SetDefault("database_uri", "postgres://postres:490Sutud@localhost:5432/gofermartUsers")
+	viper.SetDefault("accrual_system_address", "http://localhost:8000")
 
-	// runAddress := viper.GetString("run_address")
-	// databaseURI := viper.GetString("database_uri")
+	runAddress := viper.GetString("run_address")
+	databaseURI := viper.GetString("database_uri")
 	// accrualSystemAddress := viper.GetString("accrual_system_address")
 
-	if err := database.InitDB("user=postgres password=490Sutud dbname=gofermartUsers sslmode=disable"); err != nil {
+	if err := database.InitDB(databaseURI); err != nil {
 		panic(err)
 	}
 	if err := database.CreateGofermartUsersTable(); err != nil {
@@ -55,16 +57,16 @@ func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/api/user/register", user.RegisterUserHandler).Methods("POST")
-	// r.HandleFunc("/api/user/login", user.LoginUserHandler).Methods("POST")
-	// r.HandleFunc("/api/user/orders", user.UploadOrderHandler).Methods("POST")
-	// r.HandleFunc("/api/user/orders", user.GetUserOrdersHandler).Methods("GET")
-	// r.HandleFunc("/api/user/balance", user.GetUserBalanceHandler).Methods("GET")
-	// r.HandleFunc("/api/user/balance/withdraw", user.WithdrawBalanceHandler).Methods("POST")
-	// r.HandleFunc("/api/user/withdrawals", user.GetWithdrawalsHandler).Methods("GET")
-	// r.HandleFunc("/api/orders/{number}", user.GetOrderAccrualHandler).Methods("GET")
+	r.HandleFunc("/api/user/login", user.LoginUserHandler).Methods("POST")
+	r.HandleFunc("/api/user/orders", user.UploadOrderHandler).Methods("POST")
+	r.HandleFunc("/api/user/orders", user.GetUserOrdersHandler).Methods("GET")
+	r.HandleFunc("/api/user/balance", user.GetUserBalanceHandler).Methods("GET")
+	r.HandleFunc("/api/user/balance/withdraw", user.WithdrawBalanceHandler).Methods("POST")
+	r.HandleFunc("/api/user/withdrawals", user.GetWithdrawalsHandler).Methods("GET")
+	r.HandleFunc("/api/orders/{number}", user.GetOrderAccrualHandler).Methods("GET")
 
 	http.Handle("/", r)
 
 	fmt.Println("Server is listening on port 8080...")
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(runAddress, nil)
 }
